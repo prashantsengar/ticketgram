041528984b03551636103998228797d64dd5fc41
Prashant Sengar <prashantsengar5@gmail.com>
2024-08-16 13:38:34 +0530

    Update query

--- src/services/user.py
+++ src/services/user.py
@@ -1,6 +1,7 @@
 from consts import TicketStatus
 from models import SupportTicket, User, UserBan
 from peewee import DoesNotExist
+from peewee import and_
 from typing import Sequence
 
 def ban(user: User, reason: str | None = None):
@@ -26,7 +27,10 @@ def is_banned(user: User) -> bool:
 def get_open_tickets(user: User) -> Sequence[SupportTicket]:
     """Returns all open tickets created by :obj:`User`"""
     query = SupportTicket.select().where(
-        SupportTicket.user == user & SupportTicket.status == TicketStatus.OPEN
+        and_(
+            SupportTicket.user == user,
+            SupportTicket.status == TicketStatus.OPEN
+        )
     )
     print(f"Got query: {query}")
     return query.execute()
